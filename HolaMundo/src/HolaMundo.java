bbf6a113837ef9b0a5b10e11042d907d3a1458e4
Ripper2021 <95532358+Ripper2021@users.noreply.github.com>
2022-07-26 06:34:36 -0300

    Esto es una actualización del programa.

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,6 +4,6 @@ public class HolaMundo
 		public static void main (String args[])
 		{
 				System.out.println("Hola Mundo");
-			
+			System.out.println("Esto es una actualizacion del programa");
 		}
 }
