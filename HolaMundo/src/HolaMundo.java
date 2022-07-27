5c6df9616fe17ff717c33a7fc57ba9b18a99ecc9
Ripper2021 <95532358+Ripper2021@users.noreply.github.com>
2022-07-27 05:23:44 -0300

    Delete HolaMundo directory

--- HolaMundo/src/HolaMundo.java
+++
@@ -1,9 +0,0 @@
-
-public class HolaMundo 
-{
-		public static void main (String args[])
-		{
-				System.out.println("Hola Mundo");
-			System.out.println("Esto es una actualizacion del programa");
-		}
-}
